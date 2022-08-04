4eba9b5eaf77bac589fc8a2da0743532e8e1cc82
cesimiri <cesimiri@hotmail.com>
2022-08-04 12:33:06 -0400

    segundo commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import NavBar from './componentes/navBar';
+function App() {
+  return (
+    <div className="App">
+      <p>Hola</p>  
+      <NavBar/>
+      
+    </div>
+  );
+}
+
+export default App;
